10ca50469ee1e91ec6c23661ac43dabcf032aca7
宋登峰 <sky_php@qq.com>
2014-02-12 16:32:52 +0800

    数据库更新

--- update.sql
+++ update.sql
@@ -2,9 +2,17 @@
 ALTER TABLE `ask_user` ADD `supports` INT( 10 ) NOT NULL DEFAULT '0' COMMENT '赞同' AFTER `adopts` ;
 
 --2014年2月10日添加赞同表
-CREATE TABLE `ask_answer_support` (
+CREATE TABLE IF NOT EXISTS `ask_answer_support` (
  `sid` char(16) NOT NULL,
  `aid` int(10) NOT NULL,
  `time` int(10) NOT NULL,
  PRIMARY KEY (`sid`,`aid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8
+
+
+--2014年2月10日添加用户擅长分类表
+CREATE TABLE IF NOT EXISTS `ask_user_category` (
+  `uid` int(10) NOT NULL,
+  `cid` int(4) NOT NULL,
+  PRIMARY KEY (`uid`,`cid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
