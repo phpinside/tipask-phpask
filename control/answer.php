056880131fb087b43a0b84344b1980e54cef2918
宋登峰 <sky_php@qq.com>
2014-04-16 10:48:40 +0800

    评论回复调整

--- control/answer.php
+++ control/answer.php
@@ -47,11 +47,12 @@ class answercontrol extends base {
             $admin_control = ($this->user['grouptype'] == 1) ? '<span class="span-line">|</span><a href="javascript:void(0)" onclick="deletecomment({commentid},{answerid});">删除</a>' : '';
             foreach ($commentlist as $comment) {
                 $viewurl = urlmap('user/space/' . $comment['authorid'], 2);
+                $reply_control = ($this->user['uid'] != $comment['authorid']) ? '<span class="span-line">|</span><a href="javascript:void(0)" onclick="replycomment(' . $comment['authorid'] . ',' . $comment['aid'] . ');">回复</a>' : '';
                 if ($admin_control) {
                     $admin_control = str_replace("{commentid}", $comment['id'], $admin_control);
                     $admin_control = str_replace("{answerid}", $comment['aid'], $admin_control);
                 }
-                $commentstr.='<li><div class="other-comment"><a href="' . $viewurl . '" title="' . $comment['author'] . '" target="_blank" class="pic"><img width="30" height="30" src="' . $comment['avatar'] . '"  onmouseover="pop_user_on(this, \'' . $comment['authorid'] . '\', \'\');"  onmouseout="pop_user_out();"></a><p><a href="' . $viewurl . '" title="' . $comment['author'] . '" target="_blank">' . $comment['author'] . '</a>：' . $comment['content'] . '</p></div><div class="replybtn"><span class="times">' . $comment['format_time'] . '</span>' . $admin_control . '</div></li>';
+                $commentstr.='<li><div class="other-comment"><a id="comment_author_' . $comment['authorid'] . '" href="' . $viewurl . '" title="' . $comment['author'] . '" target="_blank" class="pic"><img width="30" height="30" src="' . $comment['avatar'] . '"  onmouseover="pop_user_on(this, \'' . $comment['authorid'] . '\', \'\');"  onmouseout="pop_user_out();"></a><p><a href="' . $viewurl . '" title="' . $comment['author'] . '" target="_blank">' . $comment['author'] . '</a>：' . $comment['content'] . '</p></div><div class="replybtn"><span class="times">' . $comment['format_time'] . '</span>' . $reply_control . '' . $admin_control . '</div></li>';
             }
         }
         exit($commentstr);
@@ -61,10 +62,15 @@ class answercontrol extends base {
         if (isset($this->post['content'])) {
             $content = $this->post['content'];
             $answerid = intval($this->post['answerid']);
+            $replyauthorid = intval($this->post['replyauthor']);
             $answer = $_ENV['answer']->get($answerid);
             $_ENV['answer_comment']->add($answerid, $content, $this->user['uid'], $this->user['username']);
-            if ($answer['authorid'] != $this->user['uid'])
+            if ($answer['authorid'] != $this->user['uid']){
                 $_ENV['message']->add($this->user['username'], $this->user['uid'], $answer['authorid'], '您的回答有了新评论', '您对于问题 "' . $answer['title'] . '" 的回答 "' . $answer['content'] . '" 有了新评论 "' . $content . '"<br /> <a href="' . url('question/view/' . $answer['qid'], 1) . '">点击查看</a>');
+            }
+            if ($replyauthorid && $this->user['uid']!=$replyauthorid) {
+                $_ENV['message']->add($this->user['username'], $this->user['uid'],$replyauthorid, '您的评论有了新回复', '您对于问题 "' . $answer['title'] . '" 的评论有了新回复"' . $content . '"<br /> <a href="' . url('question/view/' . $answer['qid'], 1) . '">点击查看</a>');
+            }
             exit('1');
         }
     }
